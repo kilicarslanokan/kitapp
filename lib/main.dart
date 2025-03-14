822c52870058388241f5bdd8898e4c36eb9f461a
Okan Kilicarslan <okan.kilicarslan9@gmail.com>
2025-03-14 11:57:24 +0300

    Added Riverpod state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:kitapp/views/splash_screen.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(ProviderScope(child: const MyApp()));
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
