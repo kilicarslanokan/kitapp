2b6ecd759c08cf540efc3e46f173742d353ad16c
Okan Kilicarslan <okan.kilicarslan9@gmail.com>
2025-05-11 20:57:36 +0300

    feat: added multi-language support

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:kitapp/views/home_screen.dart';
-import 'package:kitapp/views/splash_screen.dart';
+import 'package:kitapp/features/home/presentation/pages/home_screen.dart';
+import 'package:kitapp/features/auth/presentation/pages/splash_screen.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,6 +23,17 @@ class MyApp extends StatelessWidget {
       designSize: const Size(360, 690),
       builder: (context, child) {
         return MaterialApp(
+
+        localizationsDelegates: const[               // DİL DESTEĞİ
+          AppLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate
+        ],
+        supportedLocales: const[
+          Locale("en",""),
+          Locale("tr",""),
+        ],                                        // DİL DESTEĞİ
           debugShowCheckedModeBanner: false,
           home: FutureBuilder(
             future: _loginStatus(),
